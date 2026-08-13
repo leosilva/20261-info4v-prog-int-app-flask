24200f93619773513471c284ae5a50dee64619c9
Leo Silva <leo.moreira@me.com>
2026-08-13 08:31:47 -0300

    criando primeiro modelo

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import secrets
 from urllib.parse import quote_plus
 
 class Config:
-    DB_PASSWORD = "!@R00tvs3r"
+    DB_PASSWORD = "SENHA"
     SECRET_KEY = secrets.token_hex(16)
     SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://root:{quote_plus(DB_PASSWORD)}@localhost:3306/2026-info4v"
